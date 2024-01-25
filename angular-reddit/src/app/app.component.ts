946b0fccb7317f6a689b7404cc0a84352627d2a7
Muhammadrafy06 <muhammad.abdul@iisgalvanimi.edu.it>
2024-01-25 13:53:29 +0000

    added spam possibility

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, numberAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {   
+export class AppComponent { 
+  
+  
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     let t = title.value;
     let v= link.value;
     console.log(`Adding article title: `+t+ ` and link: `+ v);
     console.log(`Adding article title: `+title.value+ ` and link: `+ link.value);
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    return false;
+  }
+
+  articles = new Array<string>();
+  spamArticles(spamText: HTMLInputElement, amount: HTMLInputElement): boolean {
+    
+    console.log(`spamText: ${spamText.value} and spamNum: ${amount.value} taken as input`);
+    var thisAmount = Number(amount.value);
+    for(let i:number =0; i < thisAmount; i++)  
+    {
+        this.articles[i] = String(spamText);
+        console.log(`${this.articles[i]} on index ${i}`);
+    }
+    console.log("for loop done");
     return false;
   }
 }
